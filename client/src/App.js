95df85a4531598532db1dbe691384a83914e96dd
Anna <an9062@gmail.com>
2022-01-30 15:26:27 +1100

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/profile/:profileId" component={Profile} />
+            <Route exact path="/profile" component={Profile} />
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
           </Switch>
         </>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,25 +1,75 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import SignUpForm from "./SignupForm";
 import LoginForm from "./LoginForm";
 import Auth from "../utils/auth";
 
-import { Button, Menu } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Menu, Button } from "semantic-ui-react";
 
-const MenuExampleButtons = () => (
-  <Menu inverted size="tiny">
-    <Menu.Item>Dine with Strangers</Menu.Item>
+export default class NavBar extends Component {
+  state = {};
 
-    <Menu.Menu position="right">
-      <Menu.Item>
-        <Button primary>Sign up</Button>
-      </Menu.Item>
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-      <Menu.Item>
-        <Button>Log-in</Button>
-      </Menu.Item>
-    </Menu.Menu>
-  </Menu>
-);
+  render() {
+    const { activeItem } = this.state;
 
-export default MenuExampleButtons;
+    return (
+      <>
+        <Menu inverted borderless size="small">
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          >
+            Dine with Strangers
+          </Menu.Item>
+          <Menu.Menu position="right">
+            {/* User Logged in, show different nav items*/}
+            {Auth.loggedIn() ? (
+              <>
+                <Menu.Item
+                  name="home"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                  as={Link}
+                  to="/"
+                >
+                  Events Near Me
+                </Menu.Item>
+                <Menu.Item
+                  name="profile"
+                  active={activeItem === "profile"}
+                  onClick={this.handleItemClick}
+                  as={Link}
+                  to="/profile"
+                >
+                  My Events
+                </Menu.Item>
+
+                <Menu.Item>
+                  <Button>Log Out</Button>
+                </Menu.Item>
+              </>
+            ) : (
+              <>
+                <Menu.Item>
+                  <Modal
+                    onClose={() => setOpen(false)}
+                    onOpen={() => setOpen(true)}
+                    open={open}
+                    trigger={<Button primary>Login/Signup</Button>}
+                  >
+                    <Modal.Content>
+                      <LoginForm />
+                    </Modal.Content>
+                  </Modal>
+                </Menu.Item>
+              </>
+            )}
+          </Menu.Menu>
+        </Menu>
+      </>
+    );
+  }
+}

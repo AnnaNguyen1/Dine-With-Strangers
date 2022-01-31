f7153049ce25b5175b0c58f8bdfee948ee9c3947
Anna <an9062@gmail.com>
2022-01-31 11:44:11 +1100

    changes to navbar and homepage

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,11 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
-// import Auth from "../utils/auth";
+import Auth from "../utils/auth";
+import logo from "../images/logo.jpg";
 
 import React, { useState } from "react";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Modal, Image } from "semantic-ui-react";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
@@ -13,30 +14,52 @@ const NavBar = () => {
     <div className="navbar">
       <ul>
         <li>
-          <h1>Dine with Strangers</h1>
+          <div className="logo">
+            <Image src={logo} size="tiny" floated="left" />
+            <h1>Dine with Strangers</h1>
+          </div>
         </li>
-        <li>
-          <Modal
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={<Button>Login</Button>}
-          >
-            <Modal.Content>
-              <LoginForm handleModalClose={() => setOpen(false)} />
-            </Modal.Content>
-          </Modal>
-          <Modal
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={<Button primary>Signup</Button>}
-          >
-            <Modal.Content>
-              <SignupForm handleModalClose={() => setOpen(false)} />
-            </Modal.Content>
-          </Modal>
-        </li>
+
+        {Auth.loggedIn() ? (
+          <>
+            <li>
+              <h3 as={Link} to="/">
+                Events Near Me
+              </h3>
+            </li>
+            <li>
+              <h3 as={Link} to="/profile">
+                Profile
+              </h3>
+            </li>
+            <li>
+              <h3 onClick={Auth.logout}>Log Out</h3>
+            </li>
+          </>
+        ) : (
+          <li className="modals">
+            <Modal
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+              open={open}
+              trigger={<Button>Login</Button>}
+            >
+              <Modal.Content>
+                <LoginForm handleModalClose={() => setOpen(false)} />
+              </Modal.Content>
+            </Modal>
+            <Modal
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+              open={open}
+              trigger={<Button primary>Signup</Button>}
+            >
+              <Modal.Content>
+                <SignupForm handleModalClose={() => setOpen(false)} />
+              </Modal.Content>
+            </Modal>
+          </li>
+        )}
       </ul>
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
-// import { Image } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
+import homeImg from "../images/homeimg.jpg";
+import EventList from "../components/EventList";
+import Auth from "../utils/auth";
 
 export default function Home() {
   return (
     <div className="home">
-      <div id="home-image"></div>
+      {Auth.loggedIn() ? (
+        <EventList />
+      ) : (
+        <>
+          <div id="home-image">
+            <Image src={homeImg} floated="left" />
+          </div>
+          <EventList />
+        </>
+      )}
     </div>
   );
 }

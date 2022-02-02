4dde621c4599fee45eb7dcbd7c89bca2813aa0af
Anna <an9062@gmail.com>
2022-02-02 13:14:23 +1100

    added events to seeds and made changes to MyEvents

--- client/src/components/profile/MyEvents.js
+++ client/src/components/profile/MyEvents.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_EVENTS } from "../../utils/queries";
+import { EventCard } from "../EventCard";
+import { Card } from "semantic-ui-react";
 
 export default function MyEvents({ userData }) {
   const { data } = useQuery(QUERY_EVENTS);
@@ -14,11 +16,32 @@ export default function MyEvents({ userData }) {
   // const hasEvents = events.length  ? true : false;
   // console.log(hasEvents);
 
+  const filteredEvents = events.filter((e) => e.userId === id);
+  console.log("filtered", filteredEvents);
+
   return (
     <>
       {/**Combatting rendering issue */}
       {userData._id === undefined ? <h3>Loading....</h3> : <h3>working!</h3>}
-      {events[0] === undefined ? <h3>Loading....</h3> : <h3>working!</h3>}
+      {events[0] === undefined ? (
+        <h3>Loading....</h3>
+      ) : (
+        <Card.Group centered>
+          {filteredEvents.map((event) => {
+            return (
+              <EventCard
+                id={event._id}
+                image={event.image}
+                restaurantName={event.restaurantName}
+                restaurantAddress={event.restaurantAddress}
+                address={event.address}
+                dateTime={event.eventDate}
+                description={event.description}
+              />
+            );
+          })}
+        </Card.Group>
+      )}
     </>
   );
 }
--- server/config/seed.js
+++ server/config/seed.js
@@ -2,124 +2,65 @@ const db = require("./connection");
 const { User, Event } = require("../models");
 
 db.once("open", async () => {
-  await Event.deleteMany();
-
-  // const events = await Event.insertMany([
-  //   {
-  //     restaurantName: "Foglia di Fico",
-  //     restaurantAddress:
-  //       "585 La Trobe St (Spencer St entrance), Melbourne, Victoria 3000 Australia",
-  //     image:
-  //       "http://www.fogliadifico.com.au/wp-content/uploads/2020/05/Bottega.jpg",
-  //     eventDate: "10/02/2022",
-  //     eventTime: "6pm",
-  //     attendeeLimit: 10,
-  //     state: "VIC",
-  //     attendees: [{ _id: "1112" }, { _id: "1111" }],
-  //   },
-  //   {
-  //     restaurantName: "Hochi Mama",
-  //     restaurantAddress:
-  //       "35 Little Bourke St, Melbourne, Victoria 3000 Australia",
-  //     image:
-  //       "https://media-cdn.tripadvisor.com/media/photo-s/1c/b8/c1/6e/bigger-bolder-more-dynamic.jpg",
-  //     eventDate: "12/02/2022",
-  //     eventTime: "7pm",
-  //     attendeeLimit: 6,
-  //     state: "VIC",
-  //     attendees: [{ _id: "1111" }, { _id: "1112" }, { _id: "1113" }],
-  //   },
-  //   {
-  //     restaurantName: "Lupino Bistro and Bar",
-  //     restaurantAddress:
-  //       "41 Little Collins St, Melbourne, Victoria 3000 Australia",
-  //     image:
-  //       "https://cdn.broadsheet.com.au/cache/b7/cb/b7cb2f9b975fe194ac6c7db05ad2f36c.jpg",
-  //     eventDate: "12/03/2022",
-  //     eventTime: "7pm",
-  //     attendeeLimit: 4,
-  //     state: "VIC",
-  //     attendees: [{ _id: "1113" }],
-  //   },
-  //   {
-  //     restaurantName: "Chocolate Buddha",
-  //     restaurantAddress:
-  //       "Cnr. Flinders & Swanston Street Federation Square, Melbourne, Victoria 3000 Australia",
-  //     image:
-  //       "https://chocolatebuddha.com.au/wp-content/uploads/2020/02/Architecture-2-1024x682.jpg",
-  //     eventDate: "21/03/2022",
-  //     eventTime: "8pm",
-  //     attendeeLimit: 5,
-  //     state: "VIC",
-  //     attendees: [{ _id: "1113" }],
-  //   },
-  //   {
-  //     restaurantName: "Thai Pothong",
-  //     restaurantAddress:
-  //       "U 4 294 King St, Newtown, Sydney, New South Wales 2042 Australia",
-  //     image:
-  //       "https://cdn.concreteplayground.com/content/uploads/2020/10/Thai-Pothong-Newtown-CP-Cassandra-Hannagan-1.jpg",
-  //     eventDate: "14/02/2022",
-  //     eventTime: "6pm",
-  //     attendeeLimit: 4,
-  //     state: "NSW",
-  //     attendees: [{ _id: "1113" }],
-  //   },
-  //   {
-  //     restaurantName: "Social Brew Cafe",
-  //     restaurantAddress:
-  //       "224 Harris St, Sydney, New South Wales 2009 Australia",
-  //     image:
-  //       "https://cdn.concreteplayground.com/content/uploads/2020/10/Thai-Pothong-Newtown-CP-Cassandra-Hannagan-1.jpg",
-  //     eventDate: "12/02/2022",
-  //     eventTime: "6pm",
-  //     attendeeLimit: 4,
-  //     state: "NSW",
-  //     attendees: [{ _id: "1113" }],
-  //   },
-  // ]);
-  // console.log("events seeded");
-
   await User.deleteMany();
 
-  await User.create({
-    firstName: "Anna",
-    lastName: "Kelly",
-    email: "akelly@test.com",
-    password: "12345annakelly",
-    // favourites: [events[5]._id],
-    // attending: [events[0]._id],
-    // events: [events[1]._id],
-  });
-
-  await User.create({
-    firstName: "Pamela",
-    lastName: "Washington",
-    email: "pamwashington@test.com",
-    password: "pamtest123",
-    // favourites: [events[4]._id],
-    // attending: [events[1]._id],
-    // events: [events[0]._id],
-  });
-
-  await User.create({
-    firstName: "Elijah",
-    lastName: "Hold",
-    email: "eholt@testmail.com",
-    password: "password12345",
-    // attending: [events[1]._id],
-    // events: [events[2]._id, events[3]._id, events[4]._id, events[5]._id],
-  });
-
-  await User.create({
+  const user = await User.create({
     firstName: "Anna",
     lastName: "Nguyen",
     email: "annatest@test.com",
     password: "annatest",
-    // attending: [events[1]._id],
-    // events: [events[2]._id, events[3]._id, events[4]._id, events[5]._id],
   });
   console.log("users seeded");
+  console.log(user);
+
+  await Event.deleteMany();
+
+  const events = await Event.insertMany([
+    {
+      restaurantName: "Foglia di Fico",
+      restaurantAddress:
+        "585 La Trobe St (Spencer St entrance), Melbourne, Victoria 3000 Australia",
+      image:
+        "http://www.fogliadifico.com.au/wp-content/uploads/2020/05/Bottega.jpg",
+      eventDate: "11/11/2022T12:00:00:000Z",
+      description: "Some great Italian Food to ease the wrecking soul",
+      attendeeLimit: "5",
+      userId: user._id,
+    },
+    {
+      restaurantName: "Hochi Mama",
+      restaurantAddress:
+        "35 Little Bourke St, Melbourne, Victoria 3000 Australia",
+      image:
+        "https://media-cdn.tripadvisor.com/media/photo-s/1c/b8/c1/6e/bigger-bolder-more-dynamic.jpg",
+      eventDate: "11/11/2022T12:00:00:000Z",
+      description: "Who wouldn't love some Vietnamese food, or vietnamese ppl",
+      attendeeLimit: "6",
+      userId: user._id,
+    },
+    {
+      restaurantName: "Lupino Bistro and Bar",
+      restaurantAddress:
+        "41 Little Collins St, Melbourne, Victoria 3000 Australia",
+      image:
+        "https://cdn.broadsheet.com.au/cache/b7/cb/b7cb2f9b975fe194ac6c7db05ad2f36c.jpg",
+      eventDate: "11/11/2022T12:00:00:000Z",
+      description: "Fine dining at it's best!",
+      attendeeLimit: "4",
+      userId: user._id,
+    },
+  ]);
+  console.log("events seeded");
+  console.log(events[0]._id);
+
+  const updatedUser = await User.create({
+    firstName: "Anna",
+    lastName: "Nguyen",
+    email: "annatest1@test.com",
+    password: "annatest1",
+    favourites: [events[0]],
+  });
+  console.log(updatedUser);
 
   process.exit();
 });

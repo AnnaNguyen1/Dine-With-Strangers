5afc2a8d4f2f64b9dccc6ad9187f80e68cf67f35
Anna <an9062@gmail.com>
2022-02-04 22:33:54 +1100

    added extra data in seeds

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,11 +7,10 @@ db.once("open", async () => {
   const user = await User.create({
     firstName: "Anna",
     lastName: "Nguyen",
-    email: "annatest@test.com",
-    password: "annatest",
+    email: "annatest1@test.com",
+    password: "annatest1",
   });
-  console.log("users seeded");
-  console.log(user);
+  console.log("user seeded");
 
   await Event.deleteMany();
 
@@ -22,7 +21,7 @@ db.once("open", async () => {
         "585 La Trobe St (Spencer St entrance), Melbourne, Victoria 3000 Australia",
       image:
         "http://www.fogliadifico.com.au/wp-content/uploads/2020/05/Bottega.jpg",
-      eventDate: "11/11/2022T12:00:00:000Z",
+      eventDate: "2022-02-17T19:15",
       description: "Some great Italian Food to ease the wrecking soul",
       attendeeLimit: "5",
       userId: user._id,
@@ -33,7 +32,7 @@ db.once("open", async () => {
         "35 Little Bourke St, Melbourne, Victoria 3000 Australia",
       image:
         "https://media-cdn.tripadvisor.com/media/photo-s/1c/b8/c1/6e/bigger-bolder-more-dynamic.jpg",
-      eventDate: "11/11/2022T12:00:00:000Z",
+      eventDate: "2022-02-12T22:30",
       description: "Who wouldn't love some Vietnamese food, or vietnamese ppl",
       attendeeLimit: "6",
       userId: user._id,
@@ -44,24 +43,49 @@ db.once("open", async () => {
         "41 Little Collins St, Melbourne, Victoria 3000 Australia",
       image:
         "https://cdn.broadsheet.com.au/cache/b7/cb/b7cb2f9b975fe194ac6c7db05ad2f36c.jpg",
-      eventDate: "11/11/2022T12:00:00:000Z",
+      eventDate: "2022-02-19T18:30",
       description: "Fine dining at it's best!",
       attendeeLimit: "4",
       userId: user._id,
     },
   ]);
   console.log("events seeded");
-  console.log(events[0]._id);
 
   const updatedUser = await User.create({
     firstName: "Anna",
     lastName: "Nguyen",
-    email: "annatest1@test.com",
-    password: "annatest1",
+    email: "annatest@test.com",
+    password: "annatest",
     favourites: [events[0]],
     attending: [events[1], events[2]],
   });
-  console.log(updatedUser);
+  console.log("second user seeded");
 
+  const moreEvents = await Event.insertMany([
+    {
+      restaurantName: "Chocolate Buddha",
+      restaurantAddress:
+        "Federation Square, Swanston St & Flinders St, Melbourne VIC 3000",
+      image:
+        "https://chocolatebuddha.com.au/wp-content/uploads/2020/02/Architecture-2-1024x682.jpg",
+      eventDate: "2022-02-17T19:15",
+      description:
+        "There is a very good green tea creme brulee that I want to try. Would anyone like to join me?",
+      attendeeLimit: "5",
+      userId: updatedUser._id,
+    },
+    {
+      restaurantName: "Hungry Jacks Bourke Street",
+      restaurantAddress: "180 Bourke St, Melbourne VIC 3000",
+      image:
+        "https://www.hungryjacks.com.au/Upload/HJ/Media/Menu/product/Main/WEB_800x600_ALC_10.png",
+      eventDate: "2022-02-12T00:30",
+      description:
+        "Who wouldn't want to share some dirty burgers after a night out?!",
+      attendeeLimit: "6",
+      userId: updatedUser._id,
+    },
+  ]);
+  console.log("second user events seeded");
   process.exit();
 });

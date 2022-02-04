e8eb9eb41392f989c5573a0b6e9b010f7b591561
Anna <an9062@gmail.com>
2022-02-04 22:19:34 +1100

    redirect users to Home if token is no longer valid

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,30 +7,38 @@ import MyEvents from "../components/profile/MyEvents";
 import EventList from "../components/EventList";
 import { QUERY_ME } from "../utils/queries";
 import { useQuery } from "@apollo/client";
+import Auth from "../utils/auth";
+import Home from "./Home";
 
 export default function Profile() {
   const { data } = useQuery(QUERY_ME);
   const me = data?.me || [];
 
   return (
-    <div className="profile">
-      <h2 className="heading-profile">
-        Hi {me.firstName}!
-        <span className="lobster">
-          {" "}
-          Ready to 🍷 and dine with people you have never met before?{" "}
-        </span>
-      </h2>
+    <div>
+      {Auth.loggedIn() ? (
+        <div className="profile">
+          <h2 className="heading-profile">
+            Hi {me.firstName}!
+            <span className="lobster">
+              {" "}
+              Ready to 🍷 and dine with people you have never met before?{" "}
+            </span>
+          </h2>
 
-      <div className="tabs">
-        <Tab
-          favourites={<Favourites userData={me} />}
-          attending={<AttendEvent userData={me} />}
-          events={<MyEvents userData={me} />}
-          addevent={<EventForm userData={me} />}
-        />
-      </div>
-      <EventList userData={me} />
+          <div className="tabs">
+            <Tab
+              favourites={<Favourites userData={me} />}
+              attending={<AttendEvent userData={me} />}
+              events={<MyEvents userData={me} />}
+              addevent={<EventForm userData={me} />}
+            />
+          </div>
+          <EventList userData={me} />
+        </div>
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
